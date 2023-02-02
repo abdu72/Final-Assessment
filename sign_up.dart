c7fe8c5a418b396d461e4b27160b9e0f134c30a3
abdu72 <80330105+abdu72@users.noreply.github.com>
2023-02-02 13:56:58 +0700

    Update sign_up.dart

--- sign_up.dart
+++ sign_up.dart
@@ -1,47 +1,18 @@
-//import 'package:projectmdv/main.dart';
-
+import 'package:auth_project/auth_service.dart';
 import 'package:flutter/material.dart';
+import 'login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Sign up',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.orange,
-      ),
-      //home: LoginPage(title: 'Sign up'),
-    );
-  }
-}
-
-class SignUpPage extends StatefulWidget {
-  @override
-  const SignUpPage({Key? key, required this.title}) : super(key: key);
-  _SignUpPageState createState() => _SignUpPageState();
-  final String title;
-}
-
-class _SignUpPageState extends State<SignUpPage> {
-  final _formKey = GlobalKey<FormState>();
-
-  String? _nemail, _npassword, _name1, _name2;
-
+class SignUpPage extends StatelessWidget {
+  TextEditingController emailController = TextEditingController(text: "");
+  TextEditingController passwordController = TextEditingController(text: "");
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //appBar: AppBar(
-      //title: new Center(
-      //child: new Text(widget.title, textAlign: TextAlign.center)),
-      //),
       body: Form(
-        key: _formKey,
         child: Column(
           children: <Widget>[
-            SizedBox(height: 50),
+            SizedBox(height: 100),
             Text(
               'Sign Up',
               style: TextStyle(
@@ -57,87 +28,41 @@ class _SignUpPageState extends State<SignUpPage> {
                   color: Colors.black, fontSize: 15, letterSpacing: 1.0),
             ),
             SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                          labelText: 'First Name',
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(30)),
-                          suffixIcon: Icon(Icons.person)),
-                      validator: (input) =>
-                          !input!.contains('@') ? 'Not a valid email' : null,
-                      onSaved: (input) => _name1 = input,
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                          labelText: 'Last Name',
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(30)),
-                          suffixIcon: Icon(Icons.person)),
-                      validator: (input) =>
-                          !input!.contains('@') ? 'Not a valid email' : null,
-                      onSaved: (input) => _name2 = input,
-                    ),
-                  ),
-                ),
-              ],
-            ),
             Padding(
                 padding: const EdgeInsets.all(12.0),
                 child: TextFormField(
+                  controller: emailController,
                   decoration: InputDecoration(
-                      labelText: 'New Email',
+                      labelText: 'Email',
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(30)),
                       suffixIcon: Icon(Icons.person)),
-                  validator: (input) =>
-                      !input!.contains('@') ? 'Not a valid email' : null,
-                  onSaved: (input) => _nemail = input,
                 )),
             Padding(
                 padding: const EdgeInsets.all(12.0),
                 child: TextFormField(
+                  controller: passwordController,
                   decoration: InputDecoration(
                       labelText: 'Password',
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(30)),
-                      suffixIcon: Icon(Icons.error)),
-                  validator: (input) => input!.length < 8
-                      ? 'Must be at least 8 characters'
-                      : null,
-                  onSaved: (input) => _npassword = input,
+                      suffixIcon: Icon(Icons.lock)),
                   obscureText: true,
                 )),
             Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                      labelText: 'Verification Password',
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30)),
-                      suffixIcon: Icon(Icons.error)),
-                  validator: (input) => input!.length < 8
-                      ? 'Must be at least 8 characters'
-                      : null,
-                  onSaved: (input) => _npassword = input,
-                  obscureText: true,
-                )),
-            ElevatedButton(
-              onPressed: () {
-                _submit;
-              },
-              child: Text(
-                'Sign Up',
+              padding: const EdgeInsets.all(25.0),
+              child: TextButton(
+                onPressed: () async {
+                  await AuthServices.signUp(
+                      emailController.text, passwordController.text);
+                  //Navigator.pushNamed(context, '/signup');
+                },
+                child: Text(
+                  'Sign Up',
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
               ),
             ),
           ],
@@ -145,14 +70,4 @@ class _SignUpPageState extends State<SignUpPage> {
       ),
     );
   }
-
-  void _submit() {
-    if (_formKey.currentState!.validate()) {
-      _formKey.currentState!.save();
-      // perform login here
-      print(_nemail);
-      print(_npassword);
-    }
-  }
-
 }
